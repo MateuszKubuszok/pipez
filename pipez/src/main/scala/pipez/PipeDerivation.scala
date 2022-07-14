2a0f237d85ace73b3b63f5b721e2bdd842efcef9
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2022-07-14 13:30:50 +0200

    Documentation for PipeDerivation typeclass

--- pipez/src/main/scala/pipez/PipeDerivation.scala
+++ pipez/src/main/scala/pipez/PipeDerivation.scala
@@ -1,25 +1,43 @@
 package pipez
 
+/** Allows derivation macro to glue Pipes for `Pipe[in.field, out.field] for each field or `Pipe[In.Subtype,
+  * Out.Subtype]` for each subtype together.
+  *
+  * Assumes that `Pipe[In, Out]` is interchangeable to `(in: In, ctx: Context) => Result[Out]` where:
+  *   - `Context` is anything we want to thread through our calls (like in `ReaderT`)
+  *   - `Result[Out]` is any effect we need: `Either[Error, Out]`, `IO[Out]`
+  *
+  * If there is no need to thread anything you might define your `Context` to `Unit` and if you don't need an effect on
+  * returned value you might define `Result[Out] = Out`.
+  *
+  * @tparam Pipe
+  *   typeclass with two type parameters, used like `Pipe[In, Out]`, which is most likely a Single Abstract Method
+  *   interface, implementing some `(In, Context) => Result[Out]` function, which could be automatically generated for
+  *   `InProduct => OutProduct` or `InSumType => OutSumType` by pipeing the corresponding fields or sum type elements by
+  *   their name and combining the result together
+  */
 trait PipeDerivation[Pipe[_, _]] {
 
+  /** Type of value you want to thread through all calls */
   type Context
+
+  /** Type of value you want to return after the call */
   type Result[Out]
 
-  //  TODO: type ShouldUpdateContext <: Boolean
-
+  /** Turns a function into your `Pipe` typeclass */
   def lift[In, Out](f: (In, Context) => Result[Out]): Pipe[In, Out]
 
+  /** Calls `Pipe` as if it was a function */
   def unlift[In, Out](pipe: Pipe[In, Out])(in: In, ctx: Context): Result[Out]
 
+  /** Let you inject int information about current Path (extracted field, matched subtypes) into `Context` */
   def updateContext(context: Context, path: Path): Context
 
+  /** Wraps raw value into `Result` that `Pipe` should return */
   def pureResult[A](a: A): Result[A]
 
+  /** Combines 2 `Results` into 1 */
   def mergeResults[A, B, C](ra: Result[A], rb: => Result[B])(f: (A, B) => C): Result[C]
-
-  // TODO: traverse?
-
-  // TODO: consider putting derive and derive configured here?
 }
 object PipeDerivation extends PipeDerivationPlatform {
 
@@ -28,19 +46,23 @@ object PipeDerivation extends PipeDerivationPlatform {
     type Result[Out] = Result0[Out]
   }
 
+  /** Specialization for `Pipe`s which are interchangeable to `In => Result[Out]` */
   trait NoContext[Pipe[_, _]] extends PipeDerivation[Pipe] {
 
     final type Context = Unit
 
-    final def lift[In, Out](f: (In, Context) => Result[Out]): Pipe[In, Out] = simpleLift(in => f(in, ()))
+    /** Turns a function without `Context` into `Pipe` */
     def simpleLift[In, Out](f: In => Result[Out]): Pipe[In, Out]
+    final def lift[In, Out](f: (In, Context) => Result[Out]): Pipe[In, Out] = simpleLift(in => f(in, ()))
 
-    final def unlift[In, Out](pipe: Pipe[In, Out])(in: In, ctx: Context): Result[Out] = simpleUnlift(pipe)(in)
+    /** Calls `Pipe` as if it was a function without `Context` */
     def simpleUnlift[In, Out](pipe: Pipe[In, Out])(in: In): Result[Out]
+    final def unlift[In, Out](pipe: Pipe[In, Out])(in: In, ctx: Context): Result[Out] = simpleUnlift(pipe)(in)
 
     final def updateContext(context: Context, path: Path): Context = context
   }
 
+  /** Specialization for `Pipe`s which are interchangeable to `(In, Context) => Out` */
   trait NoParsing[Pipe[_, _]] extends PipeDerivation[Pipe] {
 
     final type Result[Out] = Out
@@ -50,5 +72,6 @@ object PipeDerivation extends PipeDerivationPlatform {
     final def mergeResults[A, B, C](ra: Result[A], rb: => Result[B])(f: (A, B) => C): Result[C] = f(ra, rb)
   }
 
+  /** Specialization for `Pipe`s which are interchangeable to `In => Out`` */
   trait Simple[Pipe[_, _]] extends NoContext[Pipe] with NoParsing[Pipe]
 }
