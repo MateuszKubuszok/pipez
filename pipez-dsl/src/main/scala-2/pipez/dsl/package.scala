e7f5079e0de2eb6627ad1d839fa7919a2907dc51
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2022-09-27 22:29:49 +0200

    Add some documentation to pipez-dsl

--- pipez-dsl/src/main/scala-2/pipez/dsl/package.scala
+++ pipez-dsl/src/main/scala-2/pipez/dsl/package.scala
@@ -6,18 +6,24 @@ package object dsl {
 
   implicit final class ConversionOps[From](private val from: From) extends AnyVal {
 
+    /** Converts values without possibility to fail using default configuration */
     @inline def convertInto[To](implicit convert: Converter[From, To]): To = convert.convert(from)
 
+    /** Converts values without possibility to fail using provided configuration */
     def convertInto[To](config: Converter.Config[From, To]): To = macro dsl.internal.Macros.deriveAndConvert[From, To]
 
+    /** Converts values with possibility to fail-fast using default configuration */
     @inline def parseFastInto[To](implicit parser: Parser[From, To]): Parser.ParsingResult[To] = parser.parseFast(from)
 
+    /** Converts values with possibility to fail-fast using provided configuration */
     def parseFastInto[To](
       config: Parser.Config[From, To]
     ): Parser.ParsingResult[To] = macro dsl.internal.Macros.deriveAndParseFast[From, To]
 
+    /** Converts values with possibility to full-error-aggregation using default configuration */
     @inline def parseFullInto[To](implicit parser: Parser[From, To]): Parser.ParsingResult[To] = parser.parseFull(from)
 
+    /** Converts values with possibility to full-error-aggregation using provided configuration */
     def parseFullInto[To](
       config: Parser.Config[From, To]
     ): Parser.ParsingResult[To] = macro dsl.internal.Macros.deriveAndParseFull[From, To]
